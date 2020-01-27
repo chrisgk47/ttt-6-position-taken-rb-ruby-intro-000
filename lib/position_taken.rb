a33ebb64499cad5c45b713229f2d1bb12fa6f3ac
Christopher Kim <chris.kim1990@gmail.com>
2020-01-27 15:31:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,7 @@
 def position_taken?(board, index)
-  if (board[index] == " " || board[index] == "" || board[index] == nil)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
   end
-
-  puts position_taken?(ttt_board, 0)
-  puts position_taken?(ttt_board, 1)
-  puts position_taken?(ttt_board, 2)
-  puts position_taken?(ttt_board, 3)
-  puts position_taken?(ttt_board, 4)
 end
