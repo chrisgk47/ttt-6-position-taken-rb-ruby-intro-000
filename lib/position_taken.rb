d41aac308e12451ef6c0f8f4d655496bfc955760
Christopher Kim <chris.kim1990@gmail.com>
2020-01-27 15:31:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,19 +1,10 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+  end
 
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def position_taken?(index)
+  puts position_taken?(ttt_board, 0)
+  puts position_taken?(ttt_board, 1)
+  puts position_taken?(ttt_board, 2)
+  puts position_taken?(ttt_board, 3)
+  puts position_taken?(ttt_board, 4)
 end
